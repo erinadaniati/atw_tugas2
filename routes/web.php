985ef9373744908e67532be5a30943aa9eed22f4
erinadaniati <erinadaniati01@gmail.com>
2020-10-24 16:24:37 +0700

    tampilan ecommerce

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/layout', function () {
+    return view('layouts/layout');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/belijus', function () {
+    return view('belijus');
+});
+
+Route::get('/testimoni', function () {
+    return view('testimoni');
+});
+
+Route::get('/kontakkami', function () {
+    return view('kontakkami');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
